5964bd0b4772a47ec58d0f990306f34c879dc3d3
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-08-25 08:30:50 -0300

    add - conexão feita

---
+++ infra/conexao.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+
+$usuario = "root";
+
+$senha = "";
+
+$banco = "pata_marcos";
+
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conexao->connect_error) {
+
+    die("Falha na conexão: " . $conexao->connect_error);
+
+};
+
+$conexao->set_charset("utf8mb4");
